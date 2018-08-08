f00adcd12faac20f1c3f3fbe3af6d8fe82fc8861
Marksman111 <wlb825077462@gmail.com>
2018-08-08 17:56:06 +0800

    wait notify 线程通信

--- src/main/java/com/marksman/reference/blog/WaitAndNotify.java
+++ src/main/java/com/marksman/reference/blog/WaitAndNotify.java
@@ -6,4 +6,36 @@ package com.marksman.reference.blog;
  * @description
  */
 public class WaitAndNotify {
+
+    public static void main(String[] args){
+        Thread t1 = new Thread(new ThreadA());
+        Thread t2 = new Thread(new ThreadB());
+
+        t1.start();
+        t2.start();
+    }
+
+    static class ThreadA implements Runnable{
+
+        @Override
+        public void run() {
+            System.out.println("A线程开始干活...");
+            try {
+                new Object().wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("A线程发出通知...");
+            new Object().notify();
+        }
+    }
+
+    static  class ThreadB implements Runnable{
+
+        @Override
+        public void run() {
+            System.out.println("B线程等待...");
+            System.out.println("B线程收到通知");
+        }
+    }
 }
